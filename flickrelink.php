1b6a5807f6d94e6087b199cfbffa427c6ced5990
eguaj <eguaj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-14 16:25:44 +0000

    tags/1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/flickrelink/tags/1.0.2@697385 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickrelink.php
+++ flickrelink.php
@@ -30,7 +30,6 @@ function flickrelink_is_link_broken($url) {
 	curl_setopt($ch, CURLOPT_NOBODY, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 15);
 
 	$data = curl_exec($ch);
